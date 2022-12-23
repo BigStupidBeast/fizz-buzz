1edd066c671f3de3720b276d6be4b977ce1711d5
Dmitrii Dobrov <60842990+BigStupidBeast@users.noreply.github.com>
2022-12-23 12:27:06 +0200

    main task

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Write a program that prints the numbers 
+from 1 to 100 
+and for multiples of ‘3’ print “Fizz”
+instead of the number and for the multiples of ‘5’ print “Buzz”.
+ 
+number divisible by 3 and 5 will
+always be divisible by 15, print
+'FizzBuzz' in place of the number
+'''
+
+
