8270d95cffaa858d85aff2f89ef62378e4083e59
Dmitrii Dobrov <60842990+BigStupidBeast@users.noreply.github.com>
2022-12-23 18:37:51 +0200

    add  main.checking_multiple  main.main_function  add number_multiple_checker.py

--- main.py
+++ main.py
@@ -10,10 +10,35 @@ always be divisible by 15, print
 '''
 
 
-for i in range(100):
-    if i%3==0:
-        print('fizz')
-    elif i%5==0:
-        print('buzz')
+def checking_multiple(number_for_checking: int, divider: int):
+    """
+    Check the condition:
+    Is number_for_checking a multiple of the divider
+    :param divider: no zero integer
+    :param number_for_checking: integer
+    :return: 1 if true else return 0
+    """
+    if number_for_checking % divider == 0:
+        return 1
     else:
-        print(i)
+        return 0
+
+
+def main_function(a: int, b: int, c: int, message1='fizz', message2='buzz'):
+    """
+    main function:
+    printing results of checking
+    takes
+    :param a: integer
+    if multiple of
+    :param b: integer
+    then print
+    :param message1: string
+    if multiple of
+    :param c: integer
+    then print
+    :param message2: string
+
+    :return: 1
+    """
+    ...
