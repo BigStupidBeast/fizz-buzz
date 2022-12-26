ba17a3709886e9a925b18620eff9be42be492488
Dmitrii Dobrov <60842990+BigStupidBeast@users.noreply.github.com>
2022-12-26 18:37:33 +0200

    0.2.1 added integer handler

--- main.py
+++ main.py
@@ -9,29 +9,71 @@ always be divisible by 15, print
 'FizzBuzz' in place of the number
 '''
 
-from number_multiple_checker import checking_multiple as checked
 
-
-def perfect_ozz_checker(num_for_check, dividers_array, message_array) -> str:
+def perfect_ozz_checker(num_for_check, dividers_array, message_array=['Fizz', 'Buzz', 'Juzz']) -> str:
     """
+    Check the number and if it multiplies one of dividers return message like “Fizz”
+
     Function takes:
     :param num_for_check: expected positive integer
-    :param dividers_array: expected array of integers
+    :param dividers_array: expected integer or array of integers
     :param message_array:  expected array of strings whose length is equal to the length
     of the array of divisors
     :return: string
     """
+
     result_of_checking = ''
+
+    if type(dividers_array) not in [int, list]:
+        raise TypeError('You must use integer or list of integers')
+
+    if type(dividers_array) == int:
+        tempo = []
+        tempo.append(dividers_array)
+        dividers_array = tempo
+
     for i in range(len(dividers_array)):
         result_of_checking += message_array[i] * (not num_for_check % dividers_array[i])
+
     if result_of_checking == '':
         result_of_checking = str(num_for_check)
+
     return result_of_checking
 
 
 if __name__ == '__main__':
 
     dividers = [3, 5, 4]
-    messages = ['fizz', 'buzz', 'juzz']
+    result_list = []
     for int_num in range(100):
-        print(perfect_ozz_checker(int_num, dividers, messages))
+        result_list.append(perfect_ozz_checker(int_num, dividers))
+    print(result_list)
+    print('**********\n')
+
+    dividers = [3, 5, 4]
+    messages = ['fizzz', 'buzzz', 'juzzz']
+    result_list = []
+    for int_num in range(100):
+        result_list.append(perfect_ozz_checker(int_num, dividers))
+    print(result_list)
+    print('**********\n')
+
+    dividers = 3
+    messages = ['fizzz', 'buzzz', 'juzzz']
+    result_list = []
+    for int_num in range(100):
+        result_list.append(perfect_ozz_checker(int_num, dividers))
+    print(result_list)
+
+'''
+smoke zero message
+smoke with message
+    main breakable cases
+smoke with tuple of message
+list with one broken part
+message length<dividers len
+dividers like string
+one divider 
+
+refactor to class
+'''
---
+++ wasteland/experiments.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+
+    divider = 3
+    if type(divider) == int:
+        tempo = []
+        tempo.append(divider)
+        divider = tempo
+    print(divider)
